5b671ec5a6f417aea4b8816c95ee291443e6cc75
Poli <diegoroca.ordes@gmail.com>
2023-07-04 08:47:47 +0200

    fixed dataset repetition in training

--- hfmtl/models.py
+++ hfmtl/models.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import numpy as np
 import torch
 import torch.nn as nn
@@ -104,6 +105,22 @@ class NLPDataCollator:
         collated = self.tasks[task_index].data_collator.__call__(features)
         collated['task']=torch.tensor([task_index])
         return collated
+=======
+from typing import Any, Dict, List, Optional, Tuple, Union
+import torch
+import numpy as np
+import evaluate
+from transformers import EvalPrediction
+from torch import nn
+from torch.utils.data.sampler import RandomSampler, WeightedRandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+from transformers.data.data_collator import InputDataClass
+from types import MappingProxyType
+from torch.utils.data import DataLoader, Dataset
+from transformers import AutoModel, AutoTokenizer
+import transformers
+from transformers.trainer_utils import EvalLoopOutput
+>>>>>>> Stashed changes
 
 class DataLoaderWithTaskname:
     def __init__(self, task_name, data_loader):
@@ -119,6 +136,17 @@ class DataLoaderWithTaskname:
         for batch in self.data_loader:
             yield batch
 
+<<<<<<< Updated upstream
+=======
+class NLPDataCollator:
+    def __init__(self, tasks):
+        self.tasks = tasks
+
+    def __call__(self, features: List[Union[InputDataClass, Dict]]) -> Dict[str, torch.Tensor]:
+        features = [{k:v for k,v in x.items() if k!='task_ids'} for x in features]
+        return features
+    
+>>>>>>> Stashed changes
 class MultitaskDataloader:
     """
     Data loader that combines and samples from multiple single-task
@@ -128,6 +156,10 @@ class MultitaskDataloader:
     def __init__(self, dataloader_dict, p=1):
         self.dataloader_dict = dataloader_dict
         N = max([len(x)**(1-p) for x in dataloader_dict.values()])
+<<<<<<< Updated upstream
+=======
+        
+>>>>>>> Stashed changes
         f_p = lambda x: int(N*x**p)
 
         self.num_batches_dict = {
@@ -162,6 +194,7 @@ class MultitaskDataloader:
             task_name = self.task_name_list[task_choice]
             yield next(dataloader_iter_dict[task_name])
 
+<<<<<<< Updated upstream
 def add_cls(model, Z_i, drop_probability=0.0):
     """model is a standard HF Transformer"""
     emb_name, emb_module = [(name,module) for name,module in model.named_modules() if isinstance(module,torch.nn.Embedding)][0]
@@ -376,15 +409,137 @@ class Trainer(transformers.Trainer):
         self.tokenizer = tokenizer
         self.processed_tasks = self.preprocess_tasks(tasks, self.tokenizer)
         
+=======
+
+class MultiTaskModel(nn.Module):
+    def __init__(self, encoder_name_or_path, tasks):
+        super().__init__()
+        
+        self.encoder = AutoModel.from_pretrained(encoder_name_or_path)
+        tokenizer_kwargs = frozendict(padding="max_length", max_length=128, truncation=True, return_tensors="pt")
+        self.tokenizer = AutoTokenizer.from_pretrained(encoder_name_or_path, **tokenizer_kwargs)
+        self.output_heads = nn.ModuleDict()
+        
+        for task in tasks:
+            task.set_tokenizer(self.tokenizer)
+            for subtask in task.y:
+                decoder = self._create_output_head(
+                    self.encoder.config.hidden_size, 
+                    task.task_type, 
+                    task.num_labels[subtask]
+                )
+                
+                self.output_heads[subtask] = decoder
+
+        self.processed_tasks = self.preprocess_tasks(tasks, self.tokenizer)
+        self.label_names = {task.name: task.label_names for task in tasks}
+        self.train_dataset = {self.processed_tasks[task.name]['train'] for task in tasks}
+        self.eval_dataset = {self.processed_tasks[task.name]['validation'] for task in tasks}
+    
+    def preprocess_tasks(self, tasks, tokenizer):      
+        features_dict = {}
+        for i, task in enumerate(tasks):
+            print("[*] Model preprocessing task", task.name)
+            
+            if hasattr(task, 'processed_features') and tokenizer == task.tokenizer:
+                features_dict[task.name] = task.processed_features
+                continue
+            
+            for split in task.dataset:
+                task.index = task.dataset[split].index = i
+            
+            features_dict[task.name] = {}
+            for phase, phase_dataset in task.dataset.items():
+                phase_dataset.index = i
+
+                features_dict[task.name][phase] = phase_dataset.map(
+                    task.preprocess_function, 
+                    batched = True,
+                    batch_size = 8,
+                    load_from_cache_file = True
+                )
+        return features_dict
+    
+    @staticmethod
+    def _create_output_head(encoder_hidden_size: int, task_type, n_labels):
+        if task_type == "TokenClassification":
+            print("[*] Creating TokenClassification head with", n_labels, "labels")
+            return TokenClassificationHead(encoder_hidden_size, n_labels)
+        else:
+            raise NotImplementedError()
+
+    def forward(self, input_ids = None, attention_mask = None, token_type_ids = None, position_ids = None,
+            head_mask = None, inputs_embeds = None, labels = None, task_ids = None, **kwargs):
+            print("FORWARD MODEL FORWARD MODEL FORWARD MODEL")
+            # compute the transformer output
+            # this is never called?
+            outputs = self.encoder(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+            )
+            sequence_output, pooled_output = outputs[:2]
+
+            print("3) Transformer has been forwarded")
+            unique_task_ids_list = torch.unique(task_ids).tolist()
+
+            loss_list = []
+            logits = None
+            # print("Computing loss...")
+            # print("task_ids", task_ids)
+            print("==> Computing loss for the following tasks:")
+            print("==>", unique_task_ids_list)
+            for unique_task_id in unique_task_ids_list:
+                print("Task_id =",unique_task_id)
+                ptc_train = self.processed_tasks['train']
+                target_cols = [col for col in ptc_train.features if col.startswith("target_")]
+                print("target_cols =", target_cols)
+
+                # for tc in target_cols:
+                #     print("Target Column =",tc)
+                #     print("Labels =",labels)
+                #     logits, task_loss = self.output_heads[str(unique_task_id)].forward(
+                #         sequence_output[task_id_filter],
+                #         pooled_output[task_id_filter],
+                #         labels = None if labels is None else labels[task_id_filter],
+                #         attention_mask=attention_mask[task_id_filter],
+                #     )
+
+                #     if labels is not None:
+                #         loss_list.append(task_loss)
+
+            # Loss averaged over all tasks
+            outputs = (logits, outputs[2:])
+            if loss_list:
+                loss = torch.stack(loss_list)
+                outputs = (loss.mean(),) + outputs
+
+            return outputs
+
+
+class MultiTaskTrainer(transformers.Trainer):
+    def __init__(self, tasks, **kwargs):
+        super().__init__(**kwargs)
+        self.p = 1
+        self.processed_tasks = self.model.processed_tasks
+        self.label_names = self.model.label_names
+>>>>>>> Stashed changes
         self.train_dataset = {
             task: dataset["train"]
             for task, dataset in self.processed_tasks.items()
         }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         self.eval_dataset = {
             task: dataset["validation"]
             for task, dataset in self.processed_tasks.items()
         }
+<<<<<<< Updated upstream
 
         self.test_dataset = {
             task: dataset["test"]
@@ -452,11 +607,32 @@ class Trainer(transformers.Trainer):
             else DistributedSampler(train_dataset)
         )
 
+=======
+        self.eval_dataset = MappingProxyType(self.eval_dataset)
+        self.tokenizer = self.model.tokenizer
+        self.pretrained_transformer = self.model.encoder
+        self.device = self.pretrained_transformer.device
+        self.data_collator = NLPDataCollator(tasks)
+        
+        print("[*] Init multitask trainer with tasks:", self.processed_tasks)
+        print("[*] Label names are:", self.label_names)
+           
+    def get_single_train_dataloader(self, task_name, train_dataset):
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+        
+        train_sampler = (SequentialSampler(train_dataset) if self.args.local_rank == -1 else DistributedSampler(train_dataset))
+>>>>>>> Stashed changes
         data_loader = DataLoaderWithTaskname(
             task_name = task_name,
             data_loader = DataLoader(
                 train_dataset,
+<<<<<<< Updated upstream
                 batch_size = self.task_batch_size(task_name),
+=======
+                batch_size = self.args.train_batch_size,
+                shuffle = False,
+>>>>>>> Stashed changes
                 sampler = train_sampler,
                 collate_fn = self.data_collator.__call__,
             ),
@@ -465,18 +641,25 @@ class Trainer(transformers.Trainer):
         return data_loader
 
     def get_train_dataloader(self):
+<<<<<<< Updated upstream
         """
         Returns a MultitaskDataloader, which is not actually a Dataloader
         but an iterable that returns a generator that samples from each
         task Dataloader
         """
+=======
+>>>>>>> Stashed changes
         return MultitaskDataloader(
             {
                 task_name: self.get_single_train_dataloader(task_name, task_dataset)
                 for task_name, task_dataset in self.train_dataset.items()
             }, p = self.p,
         )
+<<<<<<< Updated upstream
 
+=======
+    
+>>>>>>> Stashed changes
     def get_eval_dataloader(self, eval_dataset=None):
         return MultitaskDataloader(
             {
@@ -487,6 +670,7 @@ class Trainer(transformers.Trainer):
             }
         )
 
+<<<<<<< Updated upstream
     def get_test_dataloader(self, test_dataset=None):
         return MultitaskDataloader(
             {
@@ -558,3 +742,118 @@ def Model_Trainer(tasks, args):
     model = Model(tasks, args)
     trainer = Trainer(model, tasks, args)
     return model, trainer
+=======
+    def evaluation_loop(self, dataloader: DataLoader, description: str, 
+                        prediction_loss_only: bool | None = None, ignore_keys: List[str] | None = None, 
+                        metric_key_prefix: str = "eval_") -> EvalLoopOutput:
+          
+        # this is useless?
+        model = self._wrap_model(self.model, training=False, dataloader=dataloader)
+
+        for step, inputs in enumerate(dataloader):            
+            loss, preds, labels = self.prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
+            
+            for task, label_names in self.label_names.items():
+                preds_task = preds[task]
+                labels_task = labels[task]
+                
+                for label_name, labels_values in label_names.items():
+                    preds_tl  = preds_task[label_name]
+                    labels_tl = labels_task[label_name]
+                    
+                    eval_pred = EvalPrediction(
+                                predictions = preds_tl, 
+                                label_ids   = labels_tl, 
+                                inputs      = inputs)
+
+                    # compute metrics foreach head using the corresponding task eval_function
+                    # i copied the function from the task-specific class to this one
+                    metrics = self.compute_metrics_token_classification(eval_pred, label_name)
+                    metrics_eval = {}
+                    for metric in metrics.items():
+                        metrics_eval[metric_key_prefix + "_" + metric[0]] = metric[1]
+
+        return EvalLoopOutput(predictions=preds_tl, label_ids=labels_tl, metrics=metrics_eval, num_samples=len(self.eval_dataset))
+
+    def prediction_step(self, model, inputs, prediction_loss_only, ignore_keys=[]):
+        if ignore_keys is None:
+            ignore_keys = []
+
+        loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
+        loss = loss.mean().detach()
+        
+        logits_dict = {}
+        labels_dict = {}
+        for task_name, label_names in self.label_names.items():
+            logits_dict[task_name] = {}
+            labels_dict[task_name] = {}
+            for label_name in label_names:
+                logits_dict[task_name][label_name] = outputs[label_name]
+                logits_dict[task_name][label_name] = np.argmax(outputs[label_name].detach().cpu().numpy(), axis=2)
+                target_labels = []
+                for i in inputs:
+                    target_labels.append(i[label_name])
+                labels_dict[task_name][label_name] = torch.tensor(target_labels)
+        
+        return (loss, logits_dict, labels_dict)
+
+    def compute_metrics_token_classification(self, eval_pred, label_names):
+        predictions, labels = eval_pred.predictions, eval_pred.label_ids
+        
+        # for task in tasks
+        for task_name in self.label_names:
+            task_name = self.label_names['naive_absolute_n_commons']
+            true_labels = [
+                [task_name[label_names][int(l)] for l in label if l != -100] for label in labels
+            ]
+            
+            true_predictions = [
+                [task_name[label_names][p] for (p, l) in zip(prediction, label) if l != -100]
+                for prediction, label in zip(predictions, labels)
+            ]
+            metric = evaluate.load("seqeval")
+            all_metrics = metric.compute(
+                predictions = true_predictions, 
+                references = true_labels
+            )
+            
+            meta = {"name": task_name, "size": len(predictions), "index": 0}
+            print(all_metrics)
+            metrics = {k.replace("overall_",""):v for k,v in all_metrics.items() if "overall" in k}
+        
+        print(metrics)
+        return {**metrics, **meta}      
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        keys = inputs[0].keys()
+
+        input_ids = torch.tensor([i['input_ids'] for i in inputs], device=self.args.device) if 'input_ids' in keys else None
+        attention_mask = torch.tensor([i['attention_mask'] for i in inputs], device=self.args.device) if 'attention_mask' in keys else None        
+        token_type_ids = torch.tensor([i['token_type_ids'] for i in inputs], device=self.args.device) if 'token_type_ids' in keys else None        
+        position_ids = torch.tensor([i['position_ids'] for i in inputs], device=self.args.device) if 'position_ids' in keys else None        
+        head_mask = torch.tensor([i['head_mask'] for i in inputs], device=self.args.device) if 'head_mask' in keys else None        
+        inputs_embeds = torch.tensor([i['inputs_embeds'] for i in inputs], device=self.args.device) if 'inputs_embeds' in keys else None
+        
+        outputs = self.pretrained_transformer(
+            input_ids = input_ids,
+            attention_mask = attention_mask,
+            token_type_ids = token_type_ids,
+            position_ids = position_ids,
+            head_mask = head_mask,
+            inputs_embeds = inputs_embeds,
+        )
+
+        sequence_output, pooled_output = outputs[:2]
+        loss_list = []
+        logits_list = {}
+        
+        for i, head in enumerate(self.model.output_heads.values()):
+            labels_name = f"target_{i+1}"
+            labels_i = torch.tensor([i[labels_name] for i in inputs], device=self.args.device)
+            logits, loss = head(sequence_output, pooled_output, labels=labels_i, attention_mask=attention_mask)
+            loss_list.append(loss)
+            logits_list[labels_name] = logits
+        
+        loss = torch.stack(loss_list)
+        return (loss, logits_list) if return_outputs else loss
+>>>>>>> Stashed changes
--- hfmtl/tasks/token_classification.py
+++ hfmtl/tasks/token_classification.py
@@ -1,12 +1,16 @@
 import numpy as np
 from datasets import Dataset
 from transformers import DataCollatorForTokenClassification
+<<<<<<< Updated upstream
 from transformers import logging
+=======
+>>>>>>> Stashed changes
 import evaluate
 import funcy as fc
 import warnings
 from frozendict import frozendict as fdict
 from dataclasses import dataclass
+<<<<<<< Updated upstream
 from .task import Task
 
 def get_len(outputs):
@@ -24,6 +28,21 @@ class TokenClassification(Task):
     y:      str = 'target'
     num_labels: int = None
     tokenizer_kwargs: fdict = fdict(padding="max_length", max_length=256, truncation=True)
+=======
+
+@dataclass
+class TokenClassification:
+    task_type = "TokenClassification"
+    name: str = "TokenClassificationTask"
+    dataset: Dataset = None
+    metric:... = evaluate.load("seqeval")
+    main_split: str = "train"
+    tokens: str = 'tokens'
+    y: str|list = 'target'
+    num_labels: int = None
+    label_names: dict = None
+    tokenizer_kwargs: fdict = fdict(padding="max_length", max_length=128, truncation=True)
+>>>>>>> Stashed changes
 
     @staticmethod
     def _align_labels_with_tokens(labels, word_ids):
@@ -31,15 +50,22 @@ class TokenClassification(Task):
         current_word = None
         for word_id in word_ids:
             if word_id is None:
+<<<<<<< Updated upstream
                 # special token
                 new_labels.append(-100)
 
             elif word_id != current_word:
                 # new word start
+=======
+                new_labels.append(-100)
+
+            elif word_id != current_word:
+>>>>>>> Stashed changes
                 current_word = word_id
                 label = -100 if word_id is None else labels[word_id]
                 new_labels.append(label)
             
+<<<<<<< Updated upstream
             
             else:
                 # same word as previous token
@@ -50,11 +76,16 @@ class TokenClassification(Task):
                 # if label % 2 == 1:
                 #     label += 1
                 
+=======
+            else:
+                label = labels[word_id]
+>>>>>>> Stashed changes
                 new_labels.append(label)
         
         return new_labels
 
     def __post_init__(self):
+<<<<<<< Updated upstream
         warnings.filterwarnings("ignore")
         # logging.set_verbosity_error()
         super().__post_init__()
@@ -65,18 +96,39 @@ class TokenClassification(Task):
         
         self.label_names = target.feature.names
         print("[*] Number of labels found:",len(self.label_names))
+=======
+        self.label_names = {}
+        self.num_labels  = {}
+
+        for y in self.y:
+            target = self.dataset[self.main_split].features[y]
+            self.num_labels[y] = target.feature.num_classes
+            self.label_names[y] = target.feature.names if target.feature.names else [None]
+        
+        print(f"[*] TokenClassificationTask loaded {self.task_type} task with {self.num_labels} labels")
+        for k,v in self.label_names.items():
+            print(f"      {k} labels: {v}")
+
+    def get_labels(self):
+        return super().get_labels() or self.label_names
+>>>>>>> Stashed changes
 
     def set_tokenizer(self, tokenizer):
         self.tokenizer = tokenizer
         self.tokenizer.add_prefix_space = True
         self.data_collator = DataCollatorForTokenClassification(
+<<<<<<< Updated upstream
             tokenizer=self.tokenizer
+=======
+            tokenizer = self.tokenizer
+>>>>>>> Stashed changes
         )
 
     def preprocess_function(self, examples):
         if examples[self.tokens] and type(examples[self.tokens][0]) == str:
             unsqueeze, examples = True, {k:[v] for k,v in examples.items()}
         
+<<<<<<< Updated upstream
         tokenized_inputs = self.tokenizer(
             examples[self.tokens], 
             is_split_into_words=True, 
@@ -131,3 +183,29 @@ class TokenClassification(Task):
     def check(self):
         features = self.dataset['train'].features
         return self.tokens in features and self.y in features
+=======
+        def get_len(outputs):
+            try:
+                return len(outputs[fc.first(outputs)])
+            except:
+                return 1
+        
+        tokenized_inputs = self.tokenizer(
+            examples[self.tokens],
+            is_split_into_words=True,
+            **self.tokenizer_kwargs
+        )
+
+        for target_column in self.y:
+            all_labels = examples[target_column]
+            new_labels = []
+            
+            for i, labels in enumerate(all_labels):
+                word_ids = tokenized_inputs.word_ids(i)
+                new_labels.append(self._align_labels_with_tokens(labels, word_ids))
+            
+            tokenized_inputs[target_column] = new_labels        
+            tokenized_inputs['task_ids'] = [self.index]*get_len(tokenized_inputs)
+
+        return tokenized_inputs       
+>>>>>>> Stashed changes
