e18eff09e7e070246faa92d4d52add461bef05ab
Poli <diegoroca.ordes@gmail.com>
2023-07-06 10:59:19 +0200

    Create sequence_classification_head.py

---
+++ hfmtl/heads/sequence_classification_head.py
@@ -0,0 +1,42 @@
+import torch
+from torch import nn
+
+class SequenceClassificationHead(nn.Module):
+    def __init__(self, hidden_size, num_labels, dropout_p=0.1):
+        super().__init__()
+        print("SequenceClassificationHead init with hidden_size: {}, num_labels: {}, dropout_p: {}".format(hidden_size, num_labels, dropout_p))
+        self.dropout = nn.Dropout(dropout_p)
+        self.classifier = nn.Linear(hidden_size, num_labels)
+        self.num_labels = num_labels
+
+        self._init_weights()
+
+    def _init_weights(self):
+        self.classifier.weight.data.normal_(mean=0.0, std=0.02)
+        if self.classifier.bias is not None:
+            self.classifier.bias.data.zero_()
+
+    def forward(self, sequence_output, pooled_output, labels = None, attention_mask = None, **kwargs):
+        sequence_output_dropout = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output_dropout)
+
+        loss = None
+        if labels is not None:
+            loss_fct = torch.nn.CrossEntropyLoss()
+            labels = labels.long()
+
+            # Only keep active parts of the loss
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1) == 1
+                active_logits = logits.view(-1, self.num_labels)
+                active_labels = torch.where(
+                    active_loss,
+                    labels.view(-1),
+                    torch.tensor(loss_fct.ignore_index).type_as(labels),
+                )
+                print(active_logits.shape, active_labels.shape)
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        return logits, loss
