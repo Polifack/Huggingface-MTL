759b61b6fd880d5d0108e7c656805357f3e5debd
d.roca1 <d.roca1@udc.es>
2023-09-06 09:33:22 +0200

    created decode

--- hfmtl/models.py
+++ hfmtl/models.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
+import time
 import torch
 import numpy as np
 import evaluate
+import math
 from transformers import EvalPrediction
 from torch import nn
 from torch.utils.data.sampler import RandomSampler, WeightedRandomSampler, SequentialSampler
@@ -13,7 +15,7 @@ from .heads.token_classification_head import TokenClassificationHead
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoModel, AutoTokenizer
 import transformers
-from transformers.trainer_utils import EvalLoopOutput
+from transformers.trainer_utils import EvalLoopOutput, PredictionOutput, speed_metrics
 
 class DataLoaderWithTaskname:
     def __init__(self, task_name, data_loader):
@@ -83,12 +85,12 @@ class MultitaskDataloader:
 
 
 class MultiTaskModel(nn.Module):
-    def __init__(self, encoder_name_or_path, tasks):
+    def __init__(self, model_name, do_train, do_eval, do_test, tasks):
         super().__init__()
         
-        self.encoder = AutoModel.from_pretrained(encoder_name_or_path)
+        self.encoder = AutoModel.from_pretrained(model_name)
         tokenizer_kwargs = frozendict(padding="max_length", max_length=128, truncation=True, return_tensors="pt")
-        self.tokenizer = AutoTokenizer.from_pretrained(encoder_name_or_path, **tokenizer_kwargs)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
         self.output_heads = nn.ModuleDict()
         
         for task in tasks:
@@ -103,14 +105,20 @@ class MultiTaskModel(nn.Module):
                 self.output_heads[subtask] = decoder
 
         self.processed_tasks = self.preprocess_tasks(tasks, self.tokenizer)
-        self.label_names = {task.name: task.label_names for task in tasks}
-        self.train_dataset = {self.processed_tasks[task.name]['train'] for task in tasks}
-        self.eval_dataset = {self.processed_tasks[task.name]['validation'] for task in tasks}
+        self.label_names     = {task.name: task.label_names for task in tasks}
+
+        self.train_dataset   = {task.name: self.processed_tasks[task.name]['train'] for task in tasks} if do_train else None
+        self.eval_dataset    = {task.name: self.processed_tasks[task.name]['validation'] for task in tasks} if do_eval else None
+        self.test_dataset    = {task.name: self.processed_tasks[task.name]['test'] for task in tasks} if do_test else None
+
+        print("Created model with train dset=", self.train_dataset, type(self.train_dataset))
+        print("Created model with eval dset=", self.eval_dataset, type(self.eval_dataset))
+        print("Created model with test dset=", self.test_dataset, type(self.test_dataset))
     
     def preprocess_tasks(self, tasks, tokenizer):      
         features_dict = {}
         for i, task in enumerate(tasks):
-            print("[*] Model preprocessing task", task.name)
+            print("[*] Model preprocessing task: ", task.name,"***")
             
             if hasattr(task, 'processed_features') and tokenizer == task.tokenizer:
                 features_dict[task.name] = task.processed_features
@@ -123,12 +131,16 @@ class MultiTaskModel(nn.Module):
             for phase, phase_dataset in task.dataset.items():
                 phase_dataset.index = i
 
+                # fix fixed batch size
                 features_dict[task.name][phase] = phase_dataset.map(
                     task.preprocess_function, 
                     batched = True,
                     batch_size = 8,
                     load_from_cache_file = True
                 )
+
+            print("[*] Finished preprocessing task: ", task.name,"***")
+
         return features_dict
     
     @staticmethod
@@ -192,17 +204,18 @@ class MultiTaskTrainer(transformers.Trainer):
         self.p = 1
         self.processed_tasks = self.model.processed_tasks
         self.n_tasks = len(self.processed_tasks)
+        
         self.label_names = self.model.label_names
         self.task_names = list(self.processed_tasks.keys())
-        self.train_dataset = {
-            task: dataset["train"]
-            for task, dataset in self.processed_tasks.items()
-        }
-        self.eval_dataset = {
-            task: dataset["validation"]
-            for task, dataset in self.processed_tasks.items()
-        }
-        self.eval_dataset = MappingProxyType(self.eval_dataset)
+        
+        self.train_dataset = {task: dataset["train"] for task, dataset in self.processed_tasks.items()} if self.args.do_train else None
+    
+        self.eval_dataset = {task: dataset["validation"] for task, dataset in self.processed_tasks.items()} if self.args.do_eval else None
+        self.eval_dataset = MappingProxyType(self.eval_dataset) if self.eval_dataset else None
+        
+        self.test_dataset = {task: dataset["test"] for task, dataset in self.processed_tasks.items()} if self.args.do_predict else None
+        self.test_dataset = MappingProxyType(self.test_dataset) if self.test_dataset else None
+        
         self.tokenizer = self.model.tokenizer
         self.pretrained_transformer = self.model.encoder
         self.device = self.pretrained_transformer.device
@@ -210,38 +223,35 @@ class MultiTaskTrainer(transformers.Trainer):
         
         print("[*] Init multitask trainer with tasks:", self.task_names)
         print("[*] Label names are:", self.label_names)
-        
 
+    def get_single_train_dataloader(self, task_name, train_dataset):
+        if self.train_dataset is None and self.args.do_train:
+            raise ValueError("[*] Error Trainer: training requires a train_dataset.")
+        train_sampler = (SequentialSampler(train_dataset) if self.args.local_rank == -1 else DistributedSampler(train_dataset))
+        data_loader = DataLoaderWithTaskname(
+            task_name = task_name,
+            data_loader = DataLoader(
+                train_dataset,
+                batch_size = self.bsize,
+                shuffle = not self.args.do_predict, # only shuffle during training
+                sampler = train_sampler,
+                collate_fn = self.data_collator.__call__,
+            ),
+        )
 
-    
-
+        return data_loader
 
     def get_train_dataloader(self):
-        def get_single_train_dataloader(self, task_name, train_dataset):
-            if self.train_dataset is None:
-                raise ValueError("Trainer: training requires a train_dataset.")
-            train_sampler = (SequentialSampler(train_dataset) if self.args.local_rank == -1 else DistributedSampler(train_dataset))
-            data_loader = DataLoaderWithTaskname(
-                task_name = task_name,
-                data_loader = DataLoader(
-                    train_dataset,
-                    batch_size = self.args.train_batch_size,
-                    shuffle = False,
-                    sampler = train_sampler,
-                    collate_fn = self.data_collator.__call__,
-                ),
-            )
-
-            return data_loader
-        
+        self.bsize = self.args.per_device_train_batch_size
         return MultitaskDataloader(
             {
-                task_name: get_single_train_dataloader(task_name, task_dataset)
+                task_name: self.get_single_train_dataloader(task_name, task_dataset)
                 for task_name, task_dataset in self.train_dataset.items()
             }, p = self.p,
         )
     
     def get_eval_dataloader(self, eval_dataset=None):
+        self.bsize = self.args.per_device_eval_batch_size
         return MultitaskDataloader(
             {
                 task_name: self.get_single_train_dataloader(task_name, task_dataset)
@@ -251,23 +261,97 @@ class MultiTaskTrainer(transformers.Trainer):
             }
         )
 
-    def evaluation_loop(self, dataloader: DataLoader, description: str, 
-                        prediction_loss_only: bool | None = None, ignore_keys: List[str] | None = None, 
-                        metric_key_prefix: str = "eval_") -> EvalLoopOutput:
-          
-        # this is useless?
+    def get_test_dataloader(self, test_dataset=None):
+        self.bsize = self.args.per_device_eval_batch_size
+        return MultitaskDataloader(
+            {
+                task_name: self.get_single_train_dataloader(task_name, task_dataset)
+                for task_name, task_dataset in (
+                    test_dataset if test_dataset else self.test_dataset
+                ).items()
+            }
+        )
+
+    def predict(self, test_dataset: Dataset, ignore_keys: Optional[List[str]] = None, metric_key_prefix: str = "test"):
+        print("*********************************")
+        print("***     Prediction Loop       ***")
+        print("*********************************")
+
+        # memory metrics - must set up as early as possible
+        self._memory_tracker.start()
+
+        test_dataloader = self.get_test_dataloader(test_dataset)
+        start_time = time.time()
+
+        eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
+        output = eval_loop(
+            test_dataloader, 
+            description="Prediction", 
+            ignore_keys=ignore_keys, 
+            metric_key_prefix=metric_key_prefix,
+            return_preds=True
+        )
+        total_batch_size = self.args.eval_batch_size * self.args.world_size
+        if f"{metric_key_prefix}_jit_compilation_time" in output.metrics:
+            start_time += output.metrics[f"{metric_key_prefix}_jit_compilation_time"]
+            
+        output.metrics.update(
+            speed_metrics(
+                metric_key_prefix,
+                start_time,
+                num_samples=output.num_samples,
+                num_steps=math.ceil(output.num_samples / total_batch_size),
+            )
+        )
+
+        self.control = self.callback_handler.on_predict(self.args, self.state, self.control, output.metrics)
+        self._memory_tracker.stop_and_update_metrics(output.metrics)
+
+        for m in output.metrics:
+            print(m, output.metrics[m])
+
+        # restore labels
+        # for t in self.task_names:
+        #     label_names = self.label_names[t]
+        #     for sentence in label_names:
+        #         print(sentence)
+
+        # error here
+        lbls_idx = 0
+        for step, inputs in enumerate(test_dataloader):
+            print("Step:", step,"/",len(test_dataloader))
+            for seq in inputs:
+                print(seq)
+                for task in self.task_names:
+                    label_names = self.label_names[task]
+                    print(label_names[lbls_idx])
+                lbls_idx += 1
+
+        return PredictionOutput(predictions=output.predictions, label_ids=output.label_ids, metrics=output.metrics)
+
+    def evaluation_loop(self, 
+                        dataloader: DataLoader, 
+                        description: str, 
+                        prediction_loss_only: bool | None = None, 
+                        ignore_keys: List[str] | None = None, 
+                        metric_key_prefix: str = "eval_",
+                        return_preds: bool = False) -> EvalLoopOutput:
+        
         model = self._wrap_model(self.model, training=False, dataloader=dataloader)
 
-        print("*********************************")
-        print("***     Evaluation Loop       ***")
-        print("*********************************")
+        predictions = {task: [] for task in self.task_names}
+        label_ids   = {task: [] for task in self.task_names}
         for step, inputs in enumerate(dataloader):            
+            print("Step:", step,"/",len(dataloader))
+            
             loss, preds, labels = self.prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
-            print("[*] Evaluting for inputs:", inputs)
             for task in self.task_names:
                 print("    Evaluating task:", task)
                 preds_task = preds[task]
                 labels_task = labels[task]
+                
+                preds_i = {label_name: [] for label_name in self.label_names[task]}
+                lbls_i = {label_name: [] for label_name in self.label_names[task]}
                 
                 for label_name, labels_values in self.label_names[task].items():
                     print("       Evaluating target:", label_name)
@@ -285,7 +369,24 @@ class MultiTaskTrainer(transformers.Trainer):
                     for metric in metrics.items():
                         metrics_eval[metric_key_prefix + "_" + metric[0]] = metric[1]
 
-        return EvalLoopOutput(predictions=preds_tl, label_ids=labels_tl, metrics=metrics_eval, num_samples=len(self.eval_dataset))
+                    if return_preds:
+                        for sequence in labels_tl:
+                            current = []
+                            for l in sequence:
+                                if l == -100:
+                                    continue
+                                l_i = labels_values[int(l)] 
+                                current.append(l_i)
+                            lbls_i[label_name].append(current)                                
+
+                    preds_i[label_name].append(preds_tl)
+                    lbls_i[label_name].append(labels_tl)
+
+                
+                predictions[task].append(preds_i)
+                label_ids[task].append(lbls_i)
+
+        return EvalLoopOutput(predictions=predictions, label_ids=label_ids, metrics=metrics_eval, num_samples = len(dataloader))
 
     def prediction_step(self, model, inputs, prediction_loss_only, ignore_keys=[]):
         if ignore_keys is None:
@@ -308,33 +409,7 @@ class MultiTaskTrainer(transformers.Trainer):
                 labels_dict[task_name][label_name] = torch.tensor(target_labels)
         
         return (loss, logits_dict, labels_dict)
-
-    def compute_metrics_token_classification(self, eval_pred, task, label_names):
-        predictions, labels = eval_pred.predictions, eval_pred.label_ids
-        
-        # for task in tasks
-        label_values = self.label_names[task][label_names]
-        true_labels = [
-            [label_values[int(l)] for l in label if l != -100] for label in labels
-        ]
-        
-        true_predictions = [
-            [label_values[p] for (p, l) in zip(prediction, label) if l != -100]
-            for prediction, label in zip(predictions, labels)
-        ]
-        
-        #metric = evaluate.load("seqeval")
-        metric = evaluate.load('./evaluation')
-
-        all_metrics = metric.compute(
-            predictions = true_predictions, 
-            references = true_labels
-        )
-        
-        meta    = {"task_name": f"{task}_{label_names}", "size": len(predictions), "index": 0}
-        metrics = {k.replace("overall_",""):v for k,v in all_metrics.items() if "overall" in k}
-        return {**metrics, **meta}      
-
+    
     def compute_loss(self, model, inputs, return_outputs=False):
         keys = inputs[0].keys()
 
@@ -358,7 +433,6 @@ class MultiTaskTrainer(transformers.Trainer):
         loss_list = []
         logits_list = {}
         
-        
         for i, head in enumerate(self.model.output_heads.values()):
             labels_name = f"target_{i}"
             labels_i = torch.tensor([i[labels_name] for i in inputs], device=self.args.device)
@@ -367,4 +441,33 @@ class MultiTaskTrainer(transformers.Trainer):
             logits_list[labels_name] = logits
         loss = torch.stack(loss_list)
         loss = torch.mean(loss)
+        
         return (loss, logits_list) if return_outputs else loss
+
+    def compute_metrics_token_classification(self, eval_pred, task, label_names):
+        predictions, labels = eval_pred.predictions, eval_pred.label_ids
+        
+        # for task in tasks
+        label_values = self.label_names[task][label_names]
+        true_labels = [
+            [label_values[int(l)] for l in label if l != -100] for label in labels
+        ]
+        
+        true_predictions = [
+            [label_values[p] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+        
+        metric = evaluate.load("seqeval")
+        #metric = evaluate.load('./evaluation')
+
+        all_metrics = metric.compute(
+            predictions = true_predictions, 
+            references = true_labels
+        )
+        
+        meta    = {"task_name": f"{task}_{label_names}", "size": len(predictions), "index": 0}
+        metrics = {k.replace("overall_",""):v for k,v in all_metrics.items() if "overall" in k}
+        return {**metrics, **meta}      
+
+
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import json
 import argparse
 
 # disable warnings (e.g. for deprecated functions or for using non-ner datasets with ner evaluator)
-# import warnings
-# warnings.filterwarnings("ignore")
+import warnings
+warnings.filterwarnings("ignore")
 
 import datasets
 from transformers import TrainingArguments
@@ -18,10 +18,12 @@ from datasets import Sequence
 from datasets import ClassLabel
 
 def load_dset(train_path, dev_path, test_path, token_idx, label_idx):
-    def read_col_file(file_path, token_idx, label_idx):        
+    def read_col_file(file_path, token_idx, label_idx):
         with open(file_path, "r") as f:
             sentences = [[]]
-            for i,line in enumerate(f):                
+            for i, line in enumerate(f):
+                if i>2096:
+                    break
                 line = line.strip()
                 
                 if line:
@@ -43,80 +45,49 @@ def load_dset(train_path, dev_path, test_path, token_idx, label_idx):
         dataset = {
             "tokens": [[row[0] for row in sentence] for sentence in sentences],
         }
-
+        
         for i, idx in enumerate(label_idx):
             dataset[f"target_{i}"] = [[row[i+1] for row in sentence] for sentence in sentences]
 
         return dataset
 
-    train_dset = read_col_file(train_path, token_idx, label_idx)
-    train_dset = datasets.Dataset.from_dict(train_dset)
-    dev_dset = read_col_file(dev_path, token_idx, label_idx)
-    dev_dset = datasets.Dataset.from_dict(dev_dset)
+    # train_dset = read_col_file(train_path, token_idx, label_idx)
+    # train_dset = datasets.Dataset.from_dict(train_dset)
+    train_dset = None
+    
+    # dev_dset = read_col_file(dev_path, token_idx, label_idx)
+    # dev_dset = datasets.Dataset.from_dict(dev_dset)
+    dev_dset = None
+    
     test_dset = read_col_file(test_path, token_idx, label_idx)
     test_dset = datasets.Dataset.from_dict(test_dset)
 
     # Get all possible labels and cast to ClassLabel  
     for i in range(len(label_idx)):
         label_set = set()
-        for dset in [train_dset, dev_dset, test_dset]:
+        
+        #for dset in [train_dset, dev_dset, test_dset]:
+        for dset in [test_dset]:
             for labels in dset[f"target_{i}"]:
                 label_set.update(labels)
-        label_names = sorted(list(label_set))
-        print("=> label names:", label_names)
         
-        train_dset = train_dset.cast_column(f"target_{i}", Sequence(ClassLabel(names=label_names)))
-        dev_dset = dev_dset.cast_column(f"target_{i}", Sequence(ClassLabel(names=label_names)))
+        label_names = sorted(list(label_set))        
+        
+        #train_dset = train_dset.cast_column(f"target_{i}", Sequence(ClassLabel(names=label_names)))
+        #dev_dset = dev_dset.cast_column(f"target_{i}", Sequence(ClassLabel(names=label_names)))
         test_dset = test_dset.cast_column(f"target_{i}", Sequence(ClassLabel(names=label_names)))
     
     # Convert to Hugging Face DatasetDict format
     dataset = datasets.DatasetDict({
-            "train": train_dset,
-            "validation": dev_dset,
+            # "train": train_dset,
+            # "validation": dev_dset,
             "test": test_dset
         })
 
     return dataset
 
 
-if __name__ == "__main__":
-
-    args = argparse.ArgumentParser()
-    args.add_argument("--config", type=str, help="training config file")
-
-    args = args.parse_args()
-    # read train_config.json as easydict
-    with open(args.config, "r") as f:
-        args = easydict.EasyDict(json.load(f))
-
-    tasks = []
-    print("[*] Loading tasks...")
-    for task in args.tasks:
-        if task.task_type == "token_classification":              
-            tasks.append(
-                TokenClassification(
-                    dataset = load_dset(task.train_file, task.eval_file, task.test_file, task.tokens_idx, task.label_idx),
-                    name = task.task_name,
-                    y = [f"target_{i}" for i in range(len(task.label_idx))],
-                    tokenizer_kwargs = frozendict(padding="max_length", max_length=args.max_seq_length, truncation=True)
-                )
-            )
-        
-        elif task.type == "sequence_classification":               
-            tasks.append(
-                SequenceClassification(
-                    dataset = load_dset(task.train_file, task.eval_file, task.test_file, task.tokens_idx, task.label_idx),
-                    name = task.name,
-                    y = [f"target_{i}" for i in range(len(task.label_idx))],
-                    tokenizer_kwargs = frozendict(padding="max_length", max_length=args.max_seq_length, truncation=True)
-                )
-            )
-    print(f"[*] Loaded {len(tasks)} tasks")
-    print(f"[*] Tasks: {[task.name for task in tasks]}")
-
-    print("[*] Initializing model...")
-    model = MultiTaskModel(args.model_name, tasks)
-
+def train_model(model, tasks, args):
     training_args = TrainingArguments(
         output_dir = args.output_dir,
         num_train_epochs = args.num_train_epochs,
@@ -139,3 +110,84 @@ if __name__ == "__main__":
         tokenizer = model.tokenizer
     )
     trainer.train()
+
+def evaluate_model(model, tasks, args):
+    trainer_args = TrainingArguments(
+        output_dir = args.output_dir,
+        resume_from_checkpoint = args.resume_from_checkpoint,
+        do_eval= True,
+    )
+    trainer = MultiTaskTrainer(
+        model = model,
+        tasks = tasks,
+        args = trainer_args,
+        eval_dataset = model.eval_dataset,
+        compute_metrics = None,
+        tokenizer = model.tokenizer
+    )
+    trainer.evaluate()
+
+def predict_model(model, tasks, args):
+    trainer_args = TrainingArguments(output_dir=args.output_dir)
+    # override default args with args from config file
+    for k,v in args.items():
+        setattr(trainer_args, k, v) if hasattr(trainer_args, k) else None
+
+    trainer = MultiTaskTrainer(
+        model = model,
+        tasks = tasks,
+        args = trainer_args,
+        compute_metrics = None,
+        tokenizer = model.tokenizer
+    )
+
+    prediction = trainer.predict(model.test_dataset)
+
+if __name__ == "__main__":
+
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", type=str, help="training config file")
+    args = args.parse_args()
+    # read train_config.json as easydict
+    with open(args.config, "r") as f:
+        args = easydict.EasyDict(json.load(f))
+
+    tasks = []
+    print("[*] Loading tasks...")
+    for task in args.tasks:
+        if task.task_type == "token_classification":              
+            tasks.append(
+                TokenClassification(
+                    dataset = load_dset(task.train_file, task.eval_file, task.test_file, task.tokens_idx, task.label_idx),
+                    name = task.task_name,
+                    main_split = "train" if args.do_train else "test",
+                    y = [f"target_{i}" for i in range(len(task.label_idx))],
+                    tokenizer_kwargs = frozendict(padding="max_length", max_length=args.max_seq_length, truncation=True)
+                )
+            )
+        
+        elif task.type == "sequence_classification":               
+            tasks.append(
+                SequenceClassification(
+                    dataset = load_dset(task.train_file, task.eval_file, task.test_file, task.tokens_idx, task.label_idx),
+                    name = task.name,
+                    y = [f"target_{i}" for i in range(len(task.label_idx))],
+                    tokenizer_kwargs = frozendict(padding="max_length", max_length=args.max_seq_length, truncation=True)
+                )
+            )
+    print(f"[*] Loaded {len(tasks)} tasks")
+    print(f"[*] Tasks: {[task.name for task in tasks]}")
+
+    print("[*] Initializing model...")
+    model = MultiTaskModel(args.model_name, args.do_train, args.do_eval, args.do_predict, tasks)
+
+    if args.do_train:
+        print("[*] Training model...")
+        train_model(model, tasks, args)
+    if args.do_eval:
+        print("[*] Evaluating model...")
+        evaluate_model(model, tasks, args)
+    if args.do_predict:
+        print("[*] Predicting model...")
+        predict_model(model, tasks, args)
+
